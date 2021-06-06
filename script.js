82f05f60681e6b82774ab2e2c6023d0ed26355d2
cleber zacher <78827191+TioCleber@users.noreply.github.com>
2021-06-06 18:13:48 -0300

    Update script.js

--- script.js
+++ script.js
@@ -14,3 +14,20 @@ function desative(){
 }
 
 fechar.addEventListener('click', desative);
+
+
+const linksInternos = document.querySelectorAll('a[href^="#"]');
+
+function scrollToSection(event){
+    event.preventDefault();
+    const href = event.currentTarget.getAttribute('href');
+    const section = document.querySelector(href);
+    section.scrollIntoView({
+        behavior: 'smooth',
+        block: 'start',
+    });
+}
+
+linksInternos.forEach((link) => {
+    link.addEventListener('click', scrollToSection);
+});
