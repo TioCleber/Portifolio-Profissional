af3fb8999f29f948dfdfeea51fca3549f05f7a65
cleber zacher <78827191+TioCleber@users.noreply.github.com>
2021-06-07 18:45:05 -0300

    Update script.js

--- script.js
+++ script.js
@@ -21,7 +21,7 @@ const linksInternos = document.querySelectorAll('a[href^="#"]');
 function scrollToSection(event){
     event.preventDefault();
     const href = event.currentTarget.getAttribute('href');
-    const section = document.querySelector(href);
+    const section = href;
     section.scrollIntoView({
         behavior: 'smooth',
         block: 'start',
