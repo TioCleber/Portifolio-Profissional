51a20226908494a233671be9ac7c7ff923d3133e
cleber zacher <78827191+TioCleber@users.noreply.github.com>
2021-06-12 10:43:19 -0300

    Update mundo.js

--- mundo.js
+++ mundo.js
@@ -3,6 +3,7 @@ const back = document.querySelector('.js-back');
 const content = document.querySelectorAll('.js-content .h1, .js-content p');
 const max = content.length;
 let currentP = 0;
+let span = document.querySelector('span');
 
 content[currentP].classList.add('ativo');
 
@@ -15,7 +16,7 @@ function nextText(){
     if(currentP >= max){
         currentP = max - 1;
     }
-    
+    span.innerHTML = currentP;
     content[currentP].classList.add('ativo');
 }
 
@@ -28,7 +29,7 @@ function backText(){
         currentP = 0;
     }
 
-    
+    span.innerHTML = currentP;
     content[currentP].classList.add('ativo');
 }
 
